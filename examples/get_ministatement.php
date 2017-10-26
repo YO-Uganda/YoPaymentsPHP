b4bd792f1b935258602d757b67d5f6f3863d9878
Aziz Kirumira <akirumira@yo.co.ug>
2017-10-26 15:13:40 +0300

    added more scenarios to the ministatement example

--- examples/get_ministatement.php
+++ examples/get_ministatement.php
@@ -12,8 +12,8 @@ $yoAPI = new YoAPI($username, $password);
 
 
 /** 
- * This returns a statement for 26th October 2017 for all transactions of 
- * MTN mobile money that were successful
+ * This returns a statement for 26th October 2017 for all transactions 
+ * (excluding charges) of MTN mobile money that were successful
  */
 $response = $yoAPI->ac_get_ministatement('2017-10-26 00:00:00', '2017-10-26 23:59:59', 'SUCCEEDED', 'UGX-MTNMM', 0, 'TRANSACTION');
 
@@ -23,4 +23,33 @@ if($response['Status']=='OK'){
 	// This returns a PHP array of all the transactions in that period.
 }else{
 	echo "Yo Payments Error: ".$response['StatusMessage'];
+}
+
+/** 
+ * This returns a statement for 26th October 2017 for all transactions 
+ * (excluding charges) of Airtel money that were successful
+ * Note the difference is currency code 'UGX-WARIDMM'
+ */
+$airtel_response = $yoAPI->ac_get_ministatement('2017-10-26 00:00:00', '2017-10-26 23:59:59', 'SUCCEEDED', 'UGX-WARIDMM', 0, 'TRANSACTION');
+
+if($airtel_response['Status']=='OK'){
+	print_r($airtel_response['Transactions']);
+
+	// This returns a PHP array of all the transactions in that period.
+}else{
+	echo "Yo Payments Error: ".$airtel_response['StatusMessage'];
+}
+
+/** 
+ * This returns a statement for the latest 15 transactions 
+ * (including charges)
+ */
+$general_response = $yoAPI->ac_get_ministatement();
+
+if($general_response['Status']=='OK'){
+	print_r($general_response['Transactions']);
+
+	// This returns a PHP array of all the transactions in that period.
+}else{
+	echo "Yo Payments Error: ".$general_response['StatusMessage'];
 }
